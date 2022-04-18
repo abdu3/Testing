8de772efae4a2f5bd2baa41ad541dff48518d5ef
عبدالرزاق عبدالفتاح الحمري <54544954+abdu3@users.noreply.github.com>
2022-04-18 10:41:27 +0300

    Add files via upload

---
+++ testCases/parameterized/src/PrimeNumberChecker.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Abdurzaq on 18/04/2022.
+ */
+public class PrimeNumberChecker {
+    public Boolean validate(final Integer primeNumber) {
+        for (int i = 2; i < (primeNumber / 2); i++) {
+            if (primeNumber % i == 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
